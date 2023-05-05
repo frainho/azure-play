4d5365cd733a7ef103d46472fd370e5a70725667
Filipe Rainho <29108989+frainho@users.noreply.github.com>
2023-05-05 10:53:09 +0200

    Fix docker image, frontend deployed and working

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,6 +21,9 @@ provider "azurerm" {
   features {}
 }
 
+data "azurerm_subscription" "current" {
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "azure_play_rg"
   location = "westeurope"
@@ -77,4 +80,39 @@ resource "azurerm_container_registry" "acr" {
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
   admin_enabled       = true
+}
+
+# Done through cli to limit permissions to terraform which could be ok
+# resource "azurerm_role_assignment" "acr_pull_role" {
+#   principal_id         = azurerm_linux_web_app.nextjs_fe.identity[0].principal_id
+#   role_definition_name = "AcrPull"
+#   scope                = azurerm_container_registry.acr.id
+# }
+
+###### Frontend app deployment
+resource "azurerm_service_plan" "nextjs_fe_sp" {
+  name                = "nextjsfeserviceplan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
+
+resource "azurerm_linux_web_app" "nextjs_fe" {
+  name                = "nextjsfe"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.nextjs_fe_sp.location
+  service_plan_id     = azurerm_service_plan.nextjs_fe_sp.id
+
+  site_config {
+    application_stack {
+      docker_image     = "${azurerm_container_registry.acr.name}.azurecr.io/next-js"
+      docker_image_tag = "latest"
+    }
+    container_registry_use_managed_identity = true
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
